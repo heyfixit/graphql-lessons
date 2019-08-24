674676502d1f2077dc4aaf6132e0323e17a08950
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-08-24 17:45:53 -0400

    add basic custom type resolver example

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -5,12 +5,19 @@ import { GraphQLServer } from 'graphql-yoga';
 // String, Boolean, Int, Float, ID
 // as opposed to Collections - Objects and Arrays
 
+// Dummy User Data
+const users = [
+  { id: 1, name: 'User1', email: 'user1@email.com', age: 50 },
+  { id: 2, name: 'User2', email: 'user2@email.com', age: 50 },
+  { id: 3, name: 'User3', email: 'user3@email.com', age: 50 }
+];
+
 // Type Definitions AKA Application Schema
 const typeDefs = `
   type Query {
     me: User!
     post: Post!
-    greeting(name: String): String!
+    users(query: String): [User!]!
   }
 
   type User {
@@ -27,17 +34,19 @@ const typeDefs = `
   }
 `;
 
-// Resolvers
+// Resolver Functions
 const resolvers = {
   Query: {
     me: () => ({ id: 'abc123', name: 'Test', email: 'test@test.com', age: 23 }),
     post: () => ({ id: 'abc123', body: 'Test', published: true }),
-    greeting(parent, args, ctx, info) {
-      if (args.name) {
-        return `Hello ${args.name}`
+    users(parent, args, ctx, info) {
+      const { query } = args;
+      if (query) {
+        return users.filter(u =>
+          u.name.toLowerCase().includes(query.toLowerCase())
+        );
       }
-
-      return 'Hello';
+      return users;
     }
   }
 };
