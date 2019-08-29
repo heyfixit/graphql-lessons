82fbd87f8e5eb40f0cb9142a63cdfc673bc21f3b
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-08-29 05:26:06 -0400

    add relations to datamodel

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -55,3 +55,67 @@ const prisma = new Prisma({
 // prisma.query
 //   .posts(null, '{id title body published}')
 //   .then(data => console.log(JSON.stringify(data, undefined, 2)));
+
+const createPostForUser = async (authorId, data) => {
+  const userExists = await prisma.exists.User({ id: authorId });
+
+  if (!userExists) {
+    throw new Error('User not found.');
+  }
+
+  const post = await prisma.mutation.createPost(
+    {
+      data: { ...data, author: { connect: { id: authorId } } }
+    },
+    '{ id }'
+  );
+
+  const user = await prisma.query.user(
+    { where: { id: authorId } },
+    '{ id name email posts { id title published }}'
+  );
+
+  return user;
+};
+
+const updatePostForUser = async (postId, data) => {
+  const postExists = await prisma.exists.Post({ id: postId });
+
+  if (!postExists) {
+    throw new Error('Post not found.')
+  }
+
+  const postInfo = await prisma.mutation.updatePost(
+    { where: { id: postId }, data },
+    '{ author { id name email posts { id title body published} }}'
+  );
+
+  return postInfo.author;
+};
+
+// create a post for the first user
+// prisma.query
+//   .users(null, '{ id }')
+//   .then(([firstUser]) =>
+//     createPostForUser(firstUser.id, {
+//       title: 'Some Post Title',
+//       body: 'Some Post Body',
+//       published: true
+//     })
+//   )
+//   .then(data => console.log(JSON.stringify(data, undefined, 2)));
+
+// update the first post
+// prisma.query
+//   .posts(null, '{ id }')
+//   .then(([firstPost]) =>
+//     updatePostForUser(firstPost.id, { body: 'Edited Body' })
+//   )
+//   .then(data => console.log(JSON.stringify(data, undefined, 2)));
+
+// exists
+// prisma.exists
+//   .Comment({
+//     text: 'This is a comment'
+//   })
+//   .then(console.log);
