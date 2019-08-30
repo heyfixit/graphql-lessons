61aaf6d74de9b354d5b72424befdd15e05c10e6d
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-08-30 13:58:35 -0400

    use prisma for comments, restore relationship querying functionality

--- graphql-prisma/src/resolvers/Post.js
+++ graphql-prisma/src/resolvers/Post.js
@@ -1,10 +1,3 @@
-const Post = {
-  author(parent, args, { db }, info) {
-    return db.users.find(u => u.id === parent.author);
-  },
-  comments(parent, args, { db }, info) {
-    return db.comments.filter(c => c.post === parent.id);
-  }
-};
+const Post = {};
 
 export { Post as default };
--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -2,17 +2,33 @@ const Query = {
   me: () => ({ id: 'abc123', name: 'Test', email: 'test@test.com', age: 23 }),
   post: () => ({ id: 'abc123', body: 'Test', published: true }),
   users(parent, args, { prisma }, info) {
-    return prisma.query.users(null, info);
-    // const { query } = args;
-    // if (query) {
-    //   return db.users.filter(u =>
-    //     u.name.toLowerCase().includes(query.toLowerCase())
-    //   );
-    // }
-    // return db.users;
+    const opArgs = {};
+
+    if (args.query) {
+      opArgs.where = {
+        OR: [
+          {
+            name_contains: args.query
+          },
+          {
+            email_contains: args.query
+          }
+        ]
+      };
+    }
+
+    return prisma.query.users(opArgs, info);
   },
   posts(parent, args, { prisma }, info) {
-    return prisma.query.posts(null, info);
+    const opArgs = {};
+
+    if (args.query) {
+      opArgs.where = {
+        OR: [{ title_contains: args.query }, { body_contains: args.query }]
+      };
+    }
+
+    return prisma.query.posts(opArgs, info);
     // const { query } = args;
     // if (query) {
     //   return db.posts.filter(p => {
@@ -24,8 +40,8 @@ const Query = {
     //
     // return db.posts;
   },
-  comments(parent, args, { db }, info) {
-    return db.comments;
+  comments(parent, args, { db, prisma }, info) {
+    return prisma.query.comments(null, info);
   }
 };
 
